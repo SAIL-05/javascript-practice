375eae477209a1a811e3bd872a26a8cd67a2c391
mahryam08 <zulukaamariam08@gmail.com>
2025-05-06 19:34:36 +0100

    first commit

--- class.js
+++ class.js
@@ -1,152 +1,258 @@
-//strring datatype
-let mySchool = "landmark"
-console.log(mySchool)
+// //strring datatype
+// let mySchool = "landmark"
+// console.log(mySchool)
 
-let myName ="mariam"
-console.log(myName)
+// let myName ="mariam"
+// console.log(myName)
 
-let book="The girl is beautiful"
-console.log(book)
+// let book="The girl is beautiful"
+// console.log(book)
 
-//Number datatype
-var myLuckyNumber=8
-console.log(myLuckyNumber)
+// //Number datatype
+// var myLuckyNumber=8
+// console.log(myLuckyNumber)
 
- var favDate="08/07"
- console.log(favDate)
+//  var favDate="08/07"
+//  console.log(favDate)
 
-//  boolean datatype
- var me = 24
- var myBrother=  18
- console.log(me>myBrother)
+// //  boolean datatype
+//  var me = 24
+//  var myBrother=  18
+//  console.log(me>myBrother)
   
 
-//practice exercise1
-// let me = 24
-//  let myBrother=  18
-//  var higher= me>myBrother
-//  var isit=me<myBrother
-//  console.log(higher)
-//  console.log(isit)
+// //practice exercise1
+// // let me = 24
+// //  let myBrother=  18
+// //  var higher= me>myBrother
+// //  var isit=me<myBrother
+// //  console.log(higher)
+// //  console.log(isit)
 
 
- //practice exercise2
- let name = "Mariam"
-let Age = 21
-let canCodeJavascript = true
+//  //practice exercise2
+//  let name = "Mariam"
+// let Age = 21
+// let canCodeJavascript = true
 
- console.log(`hello,my name is ${name}, i am ${Age} years old and i can code javascript:${canCodeJavascript}.`)
- console.log(`hello,my name is ${name}, i am ${Age} years old and i can code javascript:${!canCodeJavascript}.`)
+//  console.log(`hello,my name is ${name}, i am ${Age} years old and i can code javascript:${canCodeJavascript}.`)
+//  console.log(`hello,my name is ${name}, i am ${Age} years old and i can code javascript:${!canCodeJavascript}.`)
 
-//practice exercise 3
-let distanceinKm = 130
-let distanceinmile = 130/1.60934
-console.log(`The distance of ${distanceinKm} kms is equal to ${distanceinmile} miles`)
+// //practice exercise 3
+// let distanceinKm = 130
+// let distanceinmile = 130/1.60934
+// console.log(`The distance of ${distanceinKm} kms is equal to ${distanceinmile} miles`)
 
-//practice exercise 4
-const heightinCm= 169
-const heightinm= 169/100
-const heightinInches= 169/2.54
-const weightinKg= 51
-const weightinPound=2.2046*51
- const bmi= weightinKg/((heightinm)*(heightinm))
- console.log(bmi)
+// //practice exercise 4
+// const heightinCm= 169
+// const heightinm= 169/100
+// const heightinInches= 169/2.54
+// const weightinKg= 51
+// const weightinPound=2.2046*51
+//  const bmi= weightinKg/((heightinm)*(heightinm))
+//  console.log(bmi)
 
-//practice exercise 5
-let g=100
-let h= 47
-const letTest= (g>h)?`yes,${g} is greater than ${h}`:`no ${g} is greater than ${h}`
-console.log(letTest)
+// //practice exercise 5
+// let g=100
+// let h= 47
+// const letTest= (g>h)?`yes,${g} is greater than ${h}`:`no ${g} is greater than ${h}`
+// console.log(letTest)
  
-// //practice exercise 6
-let score=25
+// // //practice exercise 6
+// let score=25
 
-if (score>=70){console.log("A")
-} else if(score>=60||score==69){ 
-    console.log("B")
-} else if(score>=50||score==59){
-    console.log("C")
-} else if(score>=45||score==49){
-    console.log("D")
-} else if(score>=40||score==44){
-    console.log("E")
-} else{
-    console.log("fail-F")
-}
-
-//practice exercise 7
-let y=50
-let z= 67
- 
-if (y>z){
-    console.log(`Yes,${y} is greater than ${z}`)
-}else{
-    console.log(`No,${z} is greater than ${y}`)
-}
-
-//function
-// function sayHello() {
-//     let you = prompt("what's your name? ");
-//     console.log("Hello", you + "!");
-//     console.log(`hello ${you}!`)
+// if (score>=70){console.log("A")
+// } else if(score>=60||score==69){ 
+//     console.log("B")
+// } else if(score>=50||score==59){
+//     console.log("C")
+// } else if(score>=45||score==49){
+//     console.log("D")
+// } else if(score>=40||score==44){
+//     console.log("E")
+// } else{
+//     console.log("fail-F")
 // }
-// sayHello()
+
+// //practice exercise 7
+// let y=50
+// let z= 67
+ 
+// if (y>z){
+//     console.log(`Yes,${y} is greater than ${z}`)
+// }else{
+//     console.log(`No,${z} is greater than ${y}`)
+// }
+
+// //function
+// // function sayHello() {
+// //     let you = prompt("what's your name? ");
+// //     console.log("Hello", you + "!");
+// //     console.log(`hello ${you}!`)
+// // }
+// // sayHello()
 
 
-//practice exercise 8
-function checkGreater(num1,num2){
+// //practice exercise 8
+// function checkGreater(num1,num2){
     
-    if (num1>num2){
-        console.log(`Yes,${num1} is greater than ${num2}`)
-    }else{
-        console.log(`No,${num2} is greater than ${num1}`)
+//     if (num1>num2){
+//         console.log(`Yes,${num1} is greater than ${num2}`)
+//     }else{
+//         console.log(`No,${num2} is greater than ${num1}`)
+//     }
+
+// }
+// checkGreater(30,15)
+
+// practice 9
+// function checkSum(num3,num4){
+//     console.log(num3 + num4)
+// }
+// checkSum(12,13)
+
+// //function practice 10
+// function checkGrade(score){
+//     if (score>=70){console.log("A")
+//     } else if(score>=60||score==69){ 
+//         console.log("B")
+//     } else if(score>=50||score==59){
+//         console.log("C")
+//     } else if(score>=45||score==49){
+//         console.log("D")
+//     } else if(score>=40||score==44){
+//         console.log("E")
+//     } else{
+//         console.log("fail-F")
+//     }
+// }
+// checkGrade(45)
+
+
+// //     // Conditional Statement IF, ELSE IF, ELSE/
+// //     let a = 56
+// //     let b = 32
+// //     if(a>b){
+// //         console.log("the first value is the greatest")
+// //     } else{console.log("hello")}
+// //  let valueOne = parseInt(prompt("Enter your first Number"))
+// //  let valueTwo = parseInt(prompt("Enter your second Number"))
+// //  let ans = valueOne + valueTwo
+// //  alert(ans)
+// //  console.log(ans)
+
+//  // write  a program that check if a number is even or odd
+//  let ans = parseInt(prompt("enter a number"))
+
+//   if (ans%2==0){
+// //     alert("this is an even number")
+//         console.log("this is an even number")
+// // }
+// //   else{
+// //     alert("this is an odd numer")
+//         console.log("this is an odd numer")
+// // }
+// // userNumber.toString()
+
+// // if (45>67){
+// //     console.log("45 is the highest number")
+// // }else{
+//     console.log("67 is the highest number")
+// }
+
+
+// const userNumber = parseInt(prompt("Enter a number"));
+
+// if (userNumber > 0){
+//     alert(`${userNumber} is a positive number`)
+// } else if(userNumber===0){
+//     alert(`${userNumber} is a invalid number`)
+// }
+// else if(isNAN(userNumber)){
+//     alert("this is not a number")
+// } else{
+//     alert(`${userNumber} is a negative number`)
+// }
+
+// let box1 = parseInt(prompt("enter a number"))
+// let box2 = parseInt(prompt("enter a number"))
+
+// if (box1 > box2){
+//     alert(`${box1} is greater than ${box2}`)
+// }
+//   else if(box1=== box2){
+//     alert(`${box2} is equal to ${box1}`)
+//   }
+//  else{
+//     alert(`${box2} is greater than ${box1}`)
+// }
+// let score= parseInt(prompt("Enter your score"))
+
+// if (score>=70 && score <=100){
+//     console.log("YOUR GRADE IS A")
+//     alert("YOUR GRADE IS A")
+// }
+//     else if(isNaN(score)){
+//         console.log("this is not a number")
+//         alert("this is not a number") 
+//     }
+//     else if(score>100 ){
+//     console.log("the score is invalid")
+//     alert("the score is invalid")
+// }
+//     else if(score>=60&&score==69){ 
+//     console.log("YOUR GRADE IS B")
+//     alert("YOUR GRADE IS B")
+// }
+//     else if(score>=50&&score==59){
+//     console.log("YOUR GRADE IS C")
+//     alert("YOUR GRADE IS C")
+// } 
+//     else if(score>=45&&score==49){
+//     console.log("YOUR GRADE IS D")
+//     alert("YOUR GRADE IS D")
+// }
+//      else if(score>=40&&score==44){
+//     console.log("YOUR GRADE IS E")
+//     alert("YOUR GRADE IS E")
+// }
+//     else if(score<=0){
+//         console.log("the score is invalid")
+//         alert("the score is invalid")
+//     }
+//     else{
+//     console.log("YOU fail-F")
+//     alert("YOU FAIL-F")
+// }
+
+
+const num = parseFloat(prompt("enter your height in cm"))
+const heightinm= num/100
+const weightinKg=parseFloat(prompt("enter your weight in kg")) 
+ const bmi= weightinKg/((heightinm)*(heightinm))
+
+ if (bmi<=18.5){
+        console.log("You are UNDERWEIGHT")
+        alert("You are UNDERWEIGHT")
     }
-
-}
-checkGreater(30,15)
-
-//function practice 9
-function checkSum(num3,num4){
-    console.log(num3 + num4)
-}
-checkSum(12,13)
-
-//function practice 10
-function checkGrade(score){
-    if (score>=70){console.log("A")
-    } else if(score>=60||score==69){ 
-        console.log("B")
-    } else if(score>=50||score==59){
-        console.log("C")
-    } else if(score>=45||score==49){
-        console.log("D")
-    } else if(score>=40||score==44){
-        console.log("E")
-    } else{
-        console.log("fail-F")
+        else if(bmi>=18.5&&bmi===24.9){
+            console.log("your weight is NORMAL")
+            alert("your weight is NORMAL") 
+        }
+        else if(bmi>=25 && bmi===29.9){
+        console.log("you are overweight")
+        alert("you are overweight")
     }
-}
-checkGrade(45)
-
-
-//     // Conditional Statement IF, ELSE IF, ELSE/
-//     let a = 56
-//     let b = 32
-//     if(a>b){
-//         console.log("the first value is the greatest")
-//     } else{console.log("hello")}
-//  let valueOne = parseInt(prompt("Enter your first Number"))
-//  let valueTwo = parseInt(prompt("Enter your second Number"))
-//  let ans = valueOne + valueTwo
-//  alert(ans)
-//  console.log(ans)
-
- // write  a program that check if a number is even or odd
- let ans = parseInt(prompt("enter a number"))
-
-  if (ans%2==0){
-    alert("this is an even number")
-}
-  else{
-    alert("this is an odd numer")
-}
+        else if(bmi>=30 && bmi===34.9){ 
+        console.log("your are classI OBESE")
+        alert("your are classI OBESE")
+    }
+        else if(score>=50&&score==59){
+        console.log("your are classII OBESE")
+        alert("your are classII OBESE")
+    } 
+        else{
+        console.log("your are classIII OBESE")
+        alert("your are classIII OBESE")
+        }
+ 
